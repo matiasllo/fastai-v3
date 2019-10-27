2480d862a2dec687c18566307de24fc0502d319e
matiasllo <matias.stopiello@gmail.com>
2019-10-27 19:09:12 -0300

    Update client.js

--- app/static/client.js
+++ app/static/client.js
@@ -29,7 +29,7 @@ function analyze() {
   xhr.onload = function(e) {
     if (this.readyState === 4) {
       var response = JSON.parse(e.target.responseText);
-      el("result-label").innerHTML = `Es una ${response["Result"]}`;
+      el("result-label").innerHTML = `Es una ${response['result']}`;
     }
     el("analyze-button").innerHTML = "Analizar";
   };
